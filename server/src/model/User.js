8b90625234cdd0985f8184ea0f0b33ed88ed23e7
Sagar Yadav <sagaryadav6352@gmail.com>
2025-11-06 17:09:11 +0530

    Fix typo in user schema: correct profileImgage to profileImage

--- server/src/model/User.js
+++ server/src/model/User.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
-    profileImgage: {
+    profileImage: {
       type: String,
       default: "",
     },
