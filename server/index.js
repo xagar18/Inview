572a2985d79cfa30a4ae15393290e521febe6ee7
Sagar Yadav <sagaryadav6352@gmail.com>
2025-11-06 14:45:57 +0530

    Refactor static asset serving and improve code clarity in index.js

--- server/index.js
+++ server/index.js
@@ -1,33 +1,35 @@
 import cors from "cors";
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
 import connectDB from "./src/utils/db.js";
 import { ENV } from "./src/utils/env.js";
 
 const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.use(express.json());
 app.use(cors());
 
-//db connection
+// DB connection
 connectDB();
 
 // Serve static assets in production
-const __dirname = path.resolve();
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "./client/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/dist/index.html"));
   });
 }
 
 // Example route
-app.get("/", (req, res) => {
+app.get("/hello", (req, res) => {
   res.send("Hello, World!");
 });
 
+// Use Sevella’s dynamic port
 const PORT = ENV.PORT;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
